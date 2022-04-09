a306497f537bc40aaf0c6aae05f4fecde3360e30
Ashur Kanwal <ashurkanwal@yahoo.com>
2022-04-09 11:29:55 +0100

    added django admin functionality so client admin is not able to accindently delete or add additonal header data

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
 from .models import GameInfo
 
-admin.site.register(GameInfo)
+
+class GameInfoAdmin(admin.ModelAdmin):
+  site_header = 'Header Information'
+  list_display = (
+    'name',
+    'image'
+  )
+  
+  ''' To make sure frontend not break adding admin setting for header API data not 
+      able to accidental delete or accidental add new information, 
+      only edit information on header data is allowed, so client can easily updata header 
+      content 
+  '''
+  
+  def has_add_permission(self, request, obj=None):
+    return False
+
+  def has_delete_permission(self, request, obj=None):
+    return False
+  
+  
+admin.site.register(GameInfo, GameInfoAdmin)
