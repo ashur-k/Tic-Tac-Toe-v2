fd588e5fa4d3a4d442970247ec8d040b027e1189
Ashur Kanwal <ashurkanwal@yahoo.com>
2022-04-09 10:42:46 +0100

    header component is set to load display api data header.jsx html is updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,21 +8,17 @@ const Header = ({ header_data }) => {
   console.log(header_data)
   return (
     <div className="header section-padding" id="home">
-      <div className="header-content">
+     
       {header_data.map((header) => (
-        <div>
-          {header.id}
+        <div key={header.id} className="header-content">
+          <h1 className="gradient-text">
+            {header.name}
+          </h1>
+          <p>
+             {header.description}
+          </p>
         </div>
       ))}
-        <h1 className="gradient-text">
-          Let's Play Tic-Tac-Toe
-        </h1>
-        <p>
-        Tic-Tac-Toe is a long beloved classic pen and paper or board game for two players. 
-        Variations of Tic Tac Toe have been played since the Roman Empire and 3 in a row 
-        games can be traced all the way back to ancient Egypt!
-        </p>
-      </div>
       <div className="header-image">
         <img src={logo} alt="ai"/>
       </div>        
