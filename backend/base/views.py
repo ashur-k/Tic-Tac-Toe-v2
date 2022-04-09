ce593a16b112a7c54c6c470a8ead060d41fbde01
Ashur Kanwal <ashurkanwal@yahoo.com>
2022-04-09 11:53:41 +0100

    clean views.py file / deleted unnecessary code

--- backend/base/views.py
+++ backend/base/views.py
@@ -10,16 +10,6 @@ from .models import Header
 from .serializers import HeaderSerializer
 
 
-
-ticTacToe = [
-    {
-        'id': '1',
-        'name': 'Tic Tac Toe',
-        'description': 'Tic-Tac-Toe is a long beloved classic pen and paper or board game for two players. Variations of Tic Tac Toe have been played since the Roman Empire and 3 in a row games can be traced all the way back to ancient Egypt!'
-    }
-]
-
-
 @api_view(['GET'])
 def getTicTacToe(request):
     headers = Header.objects.all()
